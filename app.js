e86f2ca9d9abf1923cb1bb96fcabc8bc293ea6db
Jelle Wallenburg <jellewallenburg@gmail.com>
2023-05-24 09:30:48 +0200

    completed but with small error

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ const express = require('express'); //require express package
 
 const app= express(); // create own server named app
 
-app.get('/home', (request, response) => {
-    console.log(request);
-    response.send('<h1>Welcome Ironhacker.');
+app.get('/', (request, response) => {
+    response.sendFile(__dirname+ '/views/index.html');
 });
 
 app.get('/about', (request, response) => {
-    console.log(request);
-    response.send('<h1>Welcome Ironhacker.');
+    response.sendFile(__dirname+ '/views/about.html');
 });
 
 app.get('/works', (request, response) => {
-    console.log(request);
-    response.send('<h1>Welcome Ironhacker.');
+    response.sendFile(__dirname+ '/views/works.html');
 });
 
 app.get('/photo', (request, response) => {
-    console.log(request);
-    response.send('<h1>Welcome Ironhacker.');
+    response.sendFile(__dirname+ '/views/photo.html');
 });
 
 
